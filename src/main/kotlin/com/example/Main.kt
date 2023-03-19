361f0dc4a5fefaa23591e792ddd86dd08c2de34f
serhii-samoilenko <serhii.samoilenko@external.quandoo.com>
2023-03-19 15:36:30 +0100

    Initial

---
+++ src/main/kotlin/com/example/Main.kt
@@ -0,0 +1,20 @@
+package com.example
+
+import io.quarkus.runtime.Quarkus
+import io.quarkus.runtime.QuarkusApplication
+import io.quarkus.runtime.annotations.QuarkusMain
+
+@QuarkusMain
+class Main : QuarkusApplication {
+    override fun run(vararg args: String): Int {
+        println("Hello " + args[1])
+        return 0
+    }
+}
+
+object JavaMain {
+    @JvmStatic
+    fun main(vararg args: String) {
+        Quarkus.run(Main::class.java, *args)
+    }
+}
